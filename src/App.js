c3816bd56f6539527f8e93cb2618791b62950d7c
Matthew Wessel <nucmatt@gmail.com>
2020-10-29 09:24:33 -0500

    Build: Add Calculator HTML
    Adds buttons and html structure for calculator. Build based on FCC
    instructions.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
-import './scss/style';
+import './scss/style.scss';
 
 function App() {
-	return <div className='App'>Hello World!</div>;
+	return (
+		<div className='container bg-primary'>
+			<main>
+				<div id='calculator'>
+					<div id='display'></div>
+					<div id='secondary-funcs'>
+						<button id='mem-clear' className='btn'>
+							MC
+						</button>
+						<button id='mem-recall' className='btn'>
+							MR
+						</button>
+						<button id='mem-add' className='btn'>
+							M+
+						</button>
+						<button id='clear-display' className='btn'>
+							AC
+						</button>
+					</div>
+					<div id='numpad'>
+						<button id='open-parentheses' className='btn'>
+							(
+						</button>
+						<button id='seven' className='btn'>
+							7
+						</button>
+						<button id='eight' className='btn'>
+							8
+						</button>
+						<button id='nine' className='btn'>
+							9
+						</button>
+						<button id='divide' className='btn'>
+							/
+						</button>
+						<button id='close-parentheses' className='btn'>
+							)
+						</button>
+						<button id='four' className='btn'>
+							4
+						</button>
+						<button id='five' className='btn'>
+							5
+						</button>
+						<button id='six' className='btn'>
+							6
+						</button>
+						<button id='multiply' className='btn'>
+							*
+						</button>
+						<button id='exponent' className='btn'>
+							y<sup>x</sup>
+						</button>
+						<button id='one' className='btn'></button>
+						<button id='two' className='btn'></button>
+						<button id='three' className='btn'></button>
+						<button id='subtract' className='btn'></button>
+						<button id='backspace' className='btn'></button>
+						<button id='zero' className='btn'></button>
+						<button id='period' className='btn'></button>
+						<button id='equals' className='btn'></button>
+						<button id='add' className='btn'></button>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
