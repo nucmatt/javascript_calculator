435e16e0b8fba4e77d82cd75cea5b67bcb8128a1
Matthew Wessel <nucmatt@gmail.com>
2021-01-06 09:06:11 -0600

    Build: Add sign change button to numbpadBtns
    Adds the sign change button to numbpadBtns list and rearranges the
    buttons there. Also moves backspace, BS, button to secondaryFuncs list
    for clarity.

--- src/App.js
+++ src/App.js
@@ -411,31 +411,31 @@ function App() {
 		console.table(state);
 	};
 
-	const secondaryFuncs = [
+	const secondaryFuncs = [		
+		{ id: 'backspace', value: 'BS' },
 		{ id: 'mem-clear', value: 'MC', actionType: 'MEM_CLEAR' },
 		{ id: 'mem-recall', value: 'MR', actionType: 'MEM_RECALL' },
 		{ id: 'mem-add', value: 'M+', actionType: 'MEM_ADD' },
 		{ id: 'clear', value: 'C', actionType: 'CLEAR_ALL' },
 	];
 	const numbpadBtns = [
-		{ id: 'parentheses', value: '()', actionType: 'PAREN_INPUT' },
 		{ id: 'seven', value: '7', actionType: 'UPDATE_EQN' },
 		{ id: 'eight', value: '8', actionType: 'UPDATE_EQN' },
 		{ id: 'nine', value: '9', actionType: 'UPDATE_EQN' },
 		{ id: 'divide', value: '/', actionType: 'OPERATOR_INPUT' },
-		// { id: 'close-parentheses', value: ')', actionType: 'CLOSEPAREN_INPUT' },
+		{ id: 'parentheses', value: '()', actionType: 'PAREN_INPUT' },
 		{ id: 'four', value: '4', actionType: 'UPDATE_EQN' },
 		{ id: 'five', value: '5', actionType: 'UPDATE_EQN' },
 		{ id: 'six', value: '6', actionType: 'UPDATE_EQN' },
 		{ id: 'multiply', value: '*', actionType: 'OPERATOR_INPUT' },
-		{ id: 'exponent', value: '^', actionType: 'OPERATOR_INPUT' },
+		{ id: 'signchange', value: '+/-', actionType: 'CHANGE_SIGN' },
 		{ id: 'one', value: '1', actionType: 'UPDATE_EQN' },
 		{ id: 'two', value: '2', actionType: 'UPDATE_EQN' },
 		{ id: 'three', value: '3', actionType: 'UPDATE_EQN' },
-		{ id: 'subtract', value: '-', actionType: 'OPERATOR_SUBTRACT' },
-		{ id: 'backspace', value: 'BS' },
-		{ id: 'zero', value: '0', actionType: 'ZERO_INPUT' },
+		{ id: 'subtract', value: '-', actionType: 'OPERATOR_INPUT' },
+		{ id: 'exponent', value: '^', actionType: 'OPERATOR_INPUT' },
 		{ id: 'decimal', value: '.', actionType: 'DECIMAL_INPUT' },
+		{ id: 'zero', value: '0', actionType: 'ZERO_INPUT' },
 		{ id: 'equals', value: '=', actionType: 'SOLVE_EQN' },
 		{ id: 'add', value: '+', actionType: 'OPERATOR_INPUT' },
 	];
@@ -450,7 +450,7 @@ function App() {
 						{solvePostfix(state.eqn)}
 					</div>
 					<div id='secondary-funcs'>
-						<div className='blank'></div>
+						{/* <div className='blank'></div> */}
 						{secondaryFuncs.map((button) => (
 							<InputBtn input={button} handleClick={handleClick} />
 						))}
